fb8f9d990ee34296b07f23de8f8dabd45e83a4ad
John Patrick Mazurek <jpmzurk@gmail.com>
2020-09-02 16:46:50 -0500

    base complete

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -10,4 +10,16 @@ const rejectUnauthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { rejectUnauthenticated };
+const rejectNonAdmirals = (req, res, next) => {
+  // check if logged in
+  if (req.user.clearance_level > 13) {
+    // They were authenticated! User may do the next thing
+    // Note! They may not be Authorized to do all things
+    next();
+  } else {
+    // failure best handled on the server. do redirect here.
+    res.sendStatus(403);
+  }
+};
+
+module.exports = { rejectUnauthenticated, rejectNonAdmirals };
--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated, rejectNonAdmirals
+} = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, rejectNonAdmirals, (req, res) => {
   console.log('req.user:', req.user);
+
+  if (req.user.clearance_level >= 4)  {
   pool
-    .query('SELECT * FROM "secret";')
+    .query(`SELECT * FROM "secret" WHERE "secrecy_level <= ${req.user.clearance_level});`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
+  }
 });
 
 module.exports = router;
